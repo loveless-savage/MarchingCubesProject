5a2fa0e7d1053bad0e338d24ef12740b1dd873f3
Andrew J <titansofalgenia@gmail.com>
2019-09-03 16:42:49 -0700

    add wrapper functions for marching cubes

--- MarchingCubes/MarchingCubes/include/marchingCubes.hpp
+++ MarchingCubes/MarchingCubes/include/marchingCubes.hpp
@@ -8,20 +8,30 @@ public:
 	// initialize MarchingCubes
 	MarchingCubes(
 		// receive parameters
-		
+		float frameSize,
+		int cellResolution
 	):
 		// initialize member variables
-		
+		frameSize(frameSize),
+		cellResolution(cellResolution)
 	{
 		// run on initialization
 		
 	};
 
-	// return final calculated model
-	OGLF::Model dumpModel();
+	// generate lookup table for per-cell primitives
+	void genLookupTable();
+
+	// generate mesh with normals from given vector field
+	void genModel();
 	
 private:
+	// frame size parameters
+	float frameSize;
+	int cellResolution;
 	
+	// lookup table
+	char lookuptable[256];
 };
 
 #endif
